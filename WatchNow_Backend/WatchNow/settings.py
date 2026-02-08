b46af15930dbfa1b3de1846563b26c8888adf872
samyaktiwari16 <samyaktiwari16@gmail.com>
2026-02-08 17:06:46 +0530

    CORS updation

--- WatchNow_Backend/WatchNow/settings.py
+++ WatchNow_Backend/WatchNow/settings.py
@@ -74,7 +74,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://watchnow-frontend.onrender.com",
+]
 
 ROOT_URLCONF = 'WatchNow.urls'
 
